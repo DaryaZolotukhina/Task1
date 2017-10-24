ab131f7558c623032b99f7a0bb90bbdc1e8ade4d
dasha.zolotukhina <d_zoolot@mail.ru>
2017-10-24 18:47:24 +0300

    Попытка3

--- Task1/Task1/Task1.cpp
+++ Task1/Task1/Task1.cpp
@@ -6,18 +6,11 @@ Hайдите такой город, сумма растояний от которого до остальных городов минимальна
 #include <fstream>
 #include <io.h>
 #include <stdio.h>
+#include <iomanip>
 
 using namespace std;
 
 const int maxPath = 100000;
-
-
-struct TFileInfo { //структура содержит файловую переменную и имя файла
-	FILE *f;
-	char FileName[20];
-
-};
-
 
 bool CheckTop(int **Matr, int Num, int n, int & sum) {
 	//реализация алгоритма Дейкстры
@@ -40,7 +33,7 @@ bool CheckTop(int **Matr, int Num, int n, int & sum) {
 	{
 		i = 0;
 		for (; i < n; i++) {//если новый город не текущий и не был посещен и расстояние до него из исходного короче, чем было
-			if (i != top && !Visited[i] && Matr[i][top]>0 && ShortPath[top] + Matr[i][top] < ShortPath[i]) {
+			if (i != top && !Visited[i] && ShortPath[top] + Matr[i][top] < ShortPath[i]) {
 				ShortPath[i] = ShortPath[top] + Matr[i][top];//обновляем длину пути к городу i
 			}
 		}
@@ -76,139 +69,6 @@ bool CheckTop(int **Matr, int Num, int n, int & sum) {
 
 }
 
-void ShowMessage(char txt[]) { //вывод сообщения пользователю
-
-	printf_s("%s\n", txt);
-	puts("");
-
-}
-
-
-bool InputQuery(char *txt) { //возвращает ответ пользователя на вопрос в формате yes\no 
-	char s[5]; //ответ пользователя
-	char ans; //первый символ ответа
-	do {
-		printf_s("%s\n", txt); //вывод вопроса 
-		gets_s(s); //считываем строку
-		ans = s[0]; //запомним первый символ ответа
-	} while (ans != 'Y' && ans != 'y' && ans != 'N' && ans != 'n'); //пока ответ не подойдет под вариант yes\no 
-	return (ans == 'Y' || ans == 'y'); //возвращем true если yes
-}
-
-
-
-bool CorrectFilename(const char *txt) //проверка на корректность имени файла, boolean //проверяет правильности имени файла
-{
-	int len; //переменная длины имени
-	bool res = true; //переменная результата выполнения функции
-	int i = 0; //переменная текущей позиции слова
-	int j;
-	char cor[8] = "/:?*<>|"; //массив некорректных символов
-	len = strlen(txt); //длина имени
-	while ((i < len) && (res)) //пока не конец имени и имя верно, выполнять цикл
-	{
-		j = 0;
-		while ((j < 8) && (res)) //пока не проверены все некоррект. символы и имя верно, выполнять цикл
-		{
-			if (txt[i] != cor[j])
-				j++; //если символ корректный, то проверяем следующий некорректный символ
-			else
-				res = false; //иначе имя некорректно
-		}
-		i++;//переходим на следующий символ имени
-	}
-	return res; //возвращаем true - имя корректно, false - имя не корректно
-}
-
-void deletestr(char *s, int l, int r) { //удаляет подстроку в строке с позиции l до r
-	char s1[30];
-	int len = strlen(s); //длина строки
-	int i = 0;
-	if (l >= 0 && l <= r && r < len) { //если отрезок правильный
-		for (; i < l; i++) //копируем строку с 0 до l-1
-			s1[i] = s[i];
-		for (int j = r + 1; j < len; j++, i++) //копирует с r+1 до len 
-			s1[i] = s[j];
-		s1[i] = '\0'; //добавляем символ конца
-		strcpy_s(s, 30, s1); //сохарняем новую строку в исходную
-	}
-
-}
-
-
-void Trim(char *str) { //удаляет пробелы слева и справа в строке
-	int i = 0;
-	int len = strlen(str); //длина строки
-	int l, r;
-	while (i<len && str[i] == ' ') i++; //доходим до символа отличного от пробела
-	l = i - 1; //запоминаем позицию 
-	i = len - 1;
-	while (i >= 0 && (str[i] == ' ' || str[i] == '\n')) i--; //с конца доходим до символа отличного от пробела
-	r = i + 1;//запоминаем
-	deletestr(str, r, len - 1); 	 //удаляем кусок с пробелами справа
-	deletestr(str, 0, l); //удаляем кусок с пробелами слева
-}
-
-bool FileExists(char *N)//проверяет существование файла
-{
-	bool res;
-
-
-	if (!_access(N, 0)) res = true; //если существует файл с указанным именем, вернет true 
-	else res = false;
-
-	return res;
-}
-
-bool InputFileName(char *txt, char *FileName, bool MustExist) { //ввод имени файла, MustExist показывает, должен ли существовать файл
-	bool check; //результат
-	cout << txt << endl; //вывод текста 
-	cin >> FileName; //считываем имя
-	Trim(FileName); //удалем пробелы слева и справа
-
-	check = true;
-
-
-	if (strlen(FileName) == 0) { //если пустая строка, выходим из функции с результатом false 
-		ShowMessage("Операция отменена");
-		check = false;
-	}
-
-	else
-		if (CorrectFilename(FileName)) { //если имя введено корректно
-			if (MustExist) //если должно существовать 
-			{
-
-				if (FileExists(FileName)) { //если существует
-
-					check = true; //результат true
-				}
-				else //не существует
-				{
-
-					ShowMessage("Файл не найден"); //вывод сообщения, результат false
-					check = false;
-				}
-			}
-			else //если не должен был существовать 
-			{
-				if (!FileExists(FileName)) { // если не существует 
-					check = true;
-				}
-
-				else //если существует 
-				{
-					if (!InputQuery("Такой файл уже существует.Заменить?")) //предлагаем заменить текущий файл новым 
-						check = false;
-				}
-			}
-		}
-		else {
-			ShowMessage("Некорректное имя файла");
-		}
-		return check; //сохраняем результат
-}
-
 int **CreateGraf(int n) {
 	int **Graf = new int*[n];
 	for (int i = 0; i < n; i++) {
@@ -229,10 +89,6 @@ bool AddElem(int **Matr, int a, int i, int j) {
 }
 
 
-
-
-
-
 int main() {
 
 	setlocale(LC_ALL, "Russian");
@@ -241,10 +97,10 @@ int main() {
 	int n;
 
 
-	cout << "Как вы хотите ввести информацию о городах? 1 - из файла, 2 - с клавиатуры" << endl;
+	cout << "Как вы хотите ввести информацию о городах? 1 - с клавиатуры, 2 - рандомно" << endl;
 	int ans;
 	cin >> ans;
-	if (ans == 2) {
+	if (ans == 1) {
 
 		cout << "Введите количество городов" << endl;
 		cin >> n;
@@ -265,46 +121,34 @@ int main() {
 	}
 	else
 	{
-		char FileName[20];
-		if (InputFileName("Введите имя файла", FileName, true)) {
-			ifstream fin(FileName);
-			fin >> n;
-			//Graf = new int*[n];
-			Graf = CreateGraf(n);
-			bool ok = true;
-			int i = 0, j = 0;
-			while (i < n - 1 && !fin.eof() && ok) {
-				j = i + 1;
-				while (j < n && !fin.eof() && ok) {
-					int a;
-					fin >> a;
-					ok = (AddElem(Graf, a, i, j));
-					j++;
-				}
-				i++;
-			}
-			if (i < n - 1) {
-				cout << "В файле недостаточно данных" << endl;
-				ok = false;
-			}
-			fin.close();
-			if (!ok)
-				return 0;
+		cout << "Введите количество городов" << endl;
+		cin >> n;
 
+		Graf = CreateGraf(n);
+		/*Автоматическая рандомизация*/
+		srand(time(0));
+		/*Ввод графа в матрицу*/
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = i + 1; j < n; j++) {
+				int a;
+				do {
+					/*Запаолнение рандомом*/
+					a=rand() % 20 + 1;
+				} while (!AddElem(Graf, a, i, j));
+
+			}
 		}
-		else
-			return 0;
 	}
 
 	//вывод матрицы 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			cout << Graf[i][j] << " ";
+			cout << setw(4) << Graf[i][j] << " ";
 		}
 		cout << endl;
 	}
 
-	//проверяем каждый город и если от него есть пути во все остальные, сравниваем сумму этих путей с минимумом, запоминая город
+	//проверяем каждый город и сравниваем сумму путей с минимумом, запоминая город
 	int sum = 0, minsum = maxPath;
 	int SearchTop = -1;
 	for (int i = 0; i < n; i++) {
